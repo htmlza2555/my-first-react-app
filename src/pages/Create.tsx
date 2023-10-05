f5dcb54ed6ebbcfb4f51a4e981072dc79559cf5e
htmlza2555 <P.nopanan@kkumail.com>
2023-10-05 13:54:58 +0700

    useNavigate()

--- src/pages/Create.tsx
+++ src/pages/Create.tsx
@@ -1,9 +1,11 @@
 import { FormEvent, useState } from 'react'
 import usePosts from '../hooks/usePosts'
 import classes from './Create.module.css'
+import { useNavigate } from 'react-router-dom'
 
 const Create = () => {
-  const { isLoading, isPosting, createPost } = usePosts()
+  const { isPosting, createPost } = usePosts()
+  const navigate = useNavigate()
   const [newTitle, setNewTitle] = useState<string>('')
   const [newBody, setNewBody] = useState<string>('')
 
@@ -15,26 +17,24 @@ const Create = () => {
 
       setNewTitle('')
       setNewBody('')
+
+      navigate('/')
     } catch (err) {
       console.error(err)
     }
   }
 
-  if (isLoading) return <h1>Loading...</h1>
-
   return (
-    <div className={classes.container}>
-      <form className={classes.postForm} onSubmit={handleSubmit}>
-        <label>Title</label>
-        <input type="text" value={newTitle} onChange={(e) => setNewTitle(e.target.value)} required />
-        <label>Body</label>
-        <input type="text" value={newBody} onChange={(e) => setNewBody(e.target.value)} required />
+    <form className={classes.postForm} onSubmit={handleSubmit}>
+      <label>Title</label>
+      <input type="text" value={newTitle} onChange={(e) => setNewTitle(e.target.value)} required />
+      <label>Body</label>
+      <input type="text" value={newBody} onChange={(e) => setNewBody(e.target.value)} required />
 
-        <button type="submit" disabled={isPosting}>
-          {isPosting ? 'Posting' : 'Post'}
-        </button>
-      </form>
-    </div>
+      <button type="submit" disabled={isPosting}>
+        {isPosting ? 'Posting' : 'Post'}
+      </button>
+    </form>
   )
 }
 
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,7 +3,9 @@ import usePosts from '../hooks/usePosts'
 import classes from './Home.module.css'
 
 const Home = () => {
-  const { posts } = usePosts()
+  const { posts, isLoading } = usePosts()
+
+  if (isLoading) return <h1>Loading...</h1>
 
   return (
     <div className={classes.container}>
